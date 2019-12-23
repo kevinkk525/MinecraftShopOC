77c471dc5daf41d0148494f358e02a83a1c77bc0
Kevin Köck <kevinkk525@users.noreply.github.com>
2019-12-23 19:30:31 +0100

    logging more flexible so it can be used in multiple projects

--- shop/logging.lua
+++ shop/logging.lua
@@ -4,17 +4,38 @@
 --- DateTime: 08.12.2019 18:28
 ---
 
-local os                   = require("os")
-local config               = require("shop.config")
-local fs                   = require("filesystem")
+local os                = require("os")
+local fs                = require("filesystem")
 
-local logfile              = io.open(config.path_logfile, "a")
-local logfile_transactions = io.open(config.path_logfile_transactions, "a")
-local colors               = { debug = 0x4B4B4B, info = 0x1E1E1E, warn = 0x332440, error = 0x660000, critical = 0xCC0040 }
+local logfile
+local logfile_transactions
+local log_lines_textbox = 1
+local path_logfile
+local path_logfile_transactions
+local max_filesize_log
 
-local log                  = {}
+local colors            = { debug = 0x4B4B4B, info = 0x1E1E1E, warn = 0x332440, error = 0x660000, critical = 0xCC0040 }
+
+local log               = {}
 local textbox
 local textbox_transactions
+
+function log.init(path, path_transactions, lines_textbox, filesize_log)
+    path_logfile              = path
+    path_logfile_transactions = path_transactions
+    max_filesize_log          = filesize_log
+    log_lines_textbox         = lines_textbox
+    logfile                   = io.open(path_logfile, "a")
+    if path_logfile_transactions ~= nil then
+        logfile_transactions = io.open(path_logfile_transactions, "a")
+        if logfile_transactions == nil then
+            print("Error opening the logfile transactions!")
+        end
+    end
+    if logfile == nil then
+        print("Error opening the logfile!")
+    end
+end
 
 local function date()
     return os.date()
@@ -24,7 +45,7 @@ function log.setTextBox(text)
     textbox = text
 end
 
-function log.setTextBoxTransactinos(text)
+function log.setTextBoxTransactions(text)
     textbox_transactions = text
 end
 
@@ -32,12 +53,12 @@ function log._log(level, message)
     logfile:write("[" .. date() .. "][" .. level .. "] " .. message .. "\n")
     if textbox then
         textbox.lines[#textbox.lines + 1] = { text = "[" .. date() .. "][" .. level .. "] " .. message, color = colors[level] }
-        if #textbox.lines > config.log_lines_textbox then
+        if #textbox.lines > log_lines_textbox then
             table.remove(textbox.lines, 1)
         end
         textbox:scrollToEnd()
     end
-    if fs.size(config.path_logfile) > config.max_filesize_log then
+    if fs.size(path_logfile) > max_filesize_log then
         print("Logfile is too big! Please notify admin")
     end
 end
@@ -66,13 +87,13 @@ function log.transaction(message)
     logfile_transactions:write("[" .. date() .. "] " .. message .. "\n")
     if textbox_transactions then
         textbox_transactions.lines[#textbox_transactions.lines + 1] = "[" .. date() .. "] " .. message
-        if #textbox_transactions.lines > config.log_lines_textbox then
+        if #textbox_transactions.lines > log_lines_textbox then
             table.remove(textbox_transactions.lines, 1)
         end
         textbox_transactions:scrollToEnd()
     end
     log.info(message)
-    if fs.size(config.path_logfile_transactions) > config.max_filesize_log then
+    if fs.size(path_logfile_transactions) > max_filesize_log then
         print("Logfile is too big! Please notify admin")
     end
 end
